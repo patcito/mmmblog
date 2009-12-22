745cacbd150e14f4ed9491b908e824fbea42256d
Patrick Aljord <patcito@gmail.com>
2009-12-21 23:23:44 -0500

    fix typo in db name

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,5 +1,5 @@
 require 'mm-paginate'
 
 MongoMapper.connection = Mongo::Connection.new(nil, nil, :auto_reconnect => true)
-MongoMapper.database = "mmmmblog-#{Rails.env}"
+MongoMapper.database = "mmmblog-#{Rails.env}"
 MongoMapper.ensure_indexes!
