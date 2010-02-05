c251d794860617ae4589ae9cc214c3dc5b86ac65
Patrick Aljord <patcito@gmail.com>
2010-02-04 21:53:52 -0500

    get rid of compass

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -21,8 +21,6 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "maruku"
   config.gem "mongo_mapper"
-  config.gem "chriseppstein-compass", :lib => "compass"
-  config.gem "chriseppstein-compass-960-plugin", :lib => "ninesixty"
   config.gem "mongomapper_ext", :version => "0.0.4", :source => "http://gemcutter.org"
   config.gem "jnunemaker-validatable", :version => "1.8.1", :lib => "validatable"
   config.gem "geoip"
